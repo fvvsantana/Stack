c3974ac06f448ac593f751a1204902343422c81b
Flavio <fvvsantana@gmail.com>
2017-08-28 23:32:27 -0300

    Create skeleton of the files

---
+++ stack.hpp
@@ -0,0 +1,18 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+
+using namespace std;
+
+namespace adt{
+    template<class T>
+        class stack{
+            private:
+
+            public:
+
+        }
+
+}
+//template implementations:
+#include "stackTemplates.hpp"
+#endif /* end of include guard: STACK_HPP */
